8d8d7dc8bfef03ce04be99e255391232f01ffaff
Stefan <Stefan@192.168.178.32>
2016-07-29 21:18:12 +0200

    near and of chapter 2

--- src/de/lindenberg/libgdx/bludbourne/BludBourne.java
+++ src/de/lindenberg/libgdx/bludbourne/BludBourne.java
@@ -1,33 +1,47 @@
 package de.lindenberg.libgdx.bludbourne;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class BludBourne extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+import de.lindenberg.libgdx.bludbourne.screens.MainGameScreen;
+
+public class BludBourne extends Game {
+	public static final MainGameScreen mainGameScreen = new MainGameScreen();
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create() {
+		setScreen(mainGameScreen);
+		
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		mainGameScreen.dispose();
+		
 	}
 }
+
+
+/*
+SpriteBatch batch;
+Texture img;
+
+@Override
+public void create () {
+	batch = new SpriteBatch();
+	img = new Texture("badlogic.jpg");
+}
+
+@Override
+public void render () {
+	Gdx.gl.glClearColor(1, 1, 0, 1);
+	Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	batch.begin();
+	batch.draw(img, 0, 0);
+	batch.end();
+}
+
+@Override
+public void dispose () {
+	batch.dispose();
+	img.dispose();
+}*/
