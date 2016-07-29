087397a6f41ec13c0acebbbfbe016710b8f3cb25
Stefan <Stefan@192.168.178.32>
2016-07-29 21:18:31 +0200

    near end of chapter 2

--- src/de/lindenberg/libgdx/bludbourne/desktop/DesktopLauncher.java
+++ src/de/lindenberg/libgdx/bludbourne/desktop/DesktopLauncher.java
@@ -1,5 +1,7 @@
 package de.lindenberg.libgdx.bludbourne.desktop;
 
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import de.lindenberg.libgdx.bludbourne.BludBourne;
@@ -7,6 +9,19 @@ import de.lindenberg.libgdx.bludbourne.BludBourne;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new BludBourne(), config);
+		
+		config.title = "BuuudBourne!";
+		config.useGL30 = false;
+		config.width = 800;
+		config.height = 600;
+		
+		
+		Application app = new LwjglApplication(new BludBourne(), config);
+		
+		Gdx.app = app;
+		//Gdx.app.setLogLevel(Application.LOG_INFO);
+		Gdx.app.setLogLevel(Application.LOG_DEBUG);
+		//Gdx.app.setLogLevel(Application.LOG_ERROR);
+		//Gdx.app.setLogLevel(Application.LOG_NONE);
 	}
 }
